4b1c14f6e5d7fad317ed7f4fb6cadad66ccaef93
knking <knr020498@gmail.com>
2020-04-03 21:15:16 +0530

    a2

--- PADHAI_assignment.py
+++ PADHAI_assignment.py
@@ -24,6 +24,44 @@
 # else:
 #     print("VERY OVERWEIGHT")
 
+#--------------------Assignment-2(fatorial)-----------------
+
+# num=int(input("Enter natural number to find factorial : "))
+# j=1
+# for i in range(1,num+1):
+#     j=j*i
+# print(j)
+
+
+#--------------------Assignmen-2(fact)
+
+
+# def fact(n):
+#     if n >1:
+#        return (n*fact(n-1))
+#     else:
+#         return 1
+# print(fact(5))
+
+#---------------------Assignment-2(3)---- division func
+
+n=int(input("Enter the value of n : "))
+def newfact(n):
+    if n>1:
+        return n* newfact(n-1)
+    else:
+        return 1
+j=newfact(n)
+
+x=float(input("Enter float value : "))
+result=float(x*n)
+finalResult=float(result/j)
+print(finalResult)
+result=eval(x*n/j)
+print(result)
+print("Facorial : ",j)
+
+
 
 
 
