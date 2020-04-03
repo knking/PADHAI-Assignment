d8a95f041d31a09a215f5826fde3cd12b7ecc5b8
knking <knr020498@gmail.com>
2020-04-03 21:10:31 +0530

    a1

---
+++ PADHAI_assignment.py
@@ -0,0 +1,31 @@
+#-----------------------------Assignment 1(BMI)---------------------
+
+# weight=int(input("Enter your weight in kg's : "))
+# preference=input("What is your preferred unit of height? Type F for feet and M for meters : ")
+#
+# if preference=="f":
+#     print("Enter your height in feat and inches")
+#     feet=int(input("Enter feat : "))
+#     inches=int(input("Now enter inches : "))
+#     meters=float(feet+inches/12)*0.3048
+#
+# else :
+#     meters=float(input("Enter your height in meters : "))
+# height=meters
+# BMI=weight/height**2
+# print(BMI)
+#
+# if BMI<18.5:
+#     print("UNDER WEIGHT")
+# elif 18.5 <= BMI < 25 :
+#     print("NORMAL")
+# elif  25 <= BMI < 30 :
+#     print("OERWEIGHT")
+# else:
+#     print("VERY OVERWEIGHT")
+
+
+
+
+
+
